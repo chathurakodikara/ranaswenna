41749f4e92aa27ba351070ebf2ddbf7ad805ebdf
samila <hasaralrandunu@gmail.com>
2020-12-29 22:26:16 +0530

    buyers edit validation done

--- app/Http/Controllers/BuyerController.php
+++ app/Http/Controllers/BuyerController.php
@@ -47,35 +47,35 @@ class BuyerController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Buyer  $buyer
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Buyer $buyer)
     {
-        //
+        return view('buyer.edit', compact('buyer'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Buyer  $buyer
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Buyer $buyer)
     {
-        //
+        $request->validate([
+
+            'business'=> 'required|min:2|max:150',
+            'registration_no'=> 'nullable|max:25',
+            'type'=> 'required|max:25',
+            'telephone'=> 'required|max:15',
+            'description'=> 'nullable|max:250',
+            'address'=> 'required|max:250',
+            'title'=> 'required|max:12|in:mr,mrs,miss,dr,ms',
+            'contacted_person'=> 'required|min:3||max:150',
+            'mobile_1'=> 'required|min:15|numeric',
+            'mobile_2'=> 'nullable|min:15|numeric',
+            'fax'=> 'nullable',
+            'email'=> 'nullable|email',
+
+         ]);
+
+         $buyer->fill($request->all())->save();
+         return redirect('/buyers');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Buyer  $buyer
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Buyer $buyer)
     {
         //
--- resources/views/buyer/edit.blade.php
+++ resources/views/buyer/edit.blade.php
@@ -2,18 +2,20 @@
     <div class="py-10">
         <div class=" max-w-4xl mx-auto ">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8 ">
-                <h3 class="font-bold text-gray-800 text-3xl flex-100 mb-4 pb-2 border-b border-gray-200">New Purchaser
+                <h3 class="font-bold text-gray-800 text-3xl flex-100 mb-4 pb-2 border-b border-gray-200">Update Purchaser
                 </h3>
 
-                <form method="POST" action="/buyers">
+                <form method="POST" action="{{ route('buyers.update', $buyer) }}">
+                    @method('put')
                     @csrf
+
                     <div class="font-bold text-sm mb-3">
                         <h2>Business Information</h2>
                     </div>
                     <div class="mb-3">
                         <x-form-label>
                             <span>Business Name *</span>
-                            <x-form-input name="business" value="{{ old('business') }}" />
+                            <x-form-input name="business" value="{{ $buyer->business }}" />
                         </x-form-label>
                         <x-jet-input-error for="business" />
                     </div>
@@ -21,7 +23,7 @@
                         <div>
                             <x-form-label>
                                 <span>Registration No </span>
-                                <x-form-input name="registration_no" value="{{ old('registration_no') }}" />
+                                <x-form-input name="registration_no" value="{{ $buyer->registration_no }}" />
                             </x-form-label>
                             <x-jet-input-error for="registration_no" />
                         </div>
@@ -29,7 +31,7 @@
                             <x-form-label>
                                 <span>Type *</span>
                                 <x-form-select name="type">
-                                    <option value="">-select-</option>
+                                    <option value="{{ $buyer->type }}">{{ $buyer->type }}</option>
                                     <option value="PVT">PVT</option>
                                     <option value="LTD">LTD</option>
                                 </x-form-select>
@@ -43,14 +45,14 @@
                         <div>
                             <x-form-label>
                                 <span> Telephone *</span>
-                                <x-form-input name="telephone" value="{{ old('telephone') }}" />
+                                <x-form-input name="telephone" value="{{ $buyer->telephone }}" />
                             </x-form-label>
                             <x-jet-input-error for="telephone" />
                         </div>
                         <div>
                             <x-form-label>
                                 <span>Fax </span>
-                                <x-form-input name="fax" value="{{ old('fax') }}" />
+                                <x-form-input name="fax" value="{{ $buyer->fax }}" />
                             </x-form-label>
                             <x-jet-input-error for="fax" />
                         </div>
@@ -59,7 +61,7 @@
                     <div class="mb-3">
                         <x-form-label>
                             <span>Description</span>
-                            <x-form-textarea name="description">{{ old('description') }}</x-form-textarea>
+                            <x-form-textarea name="description">{{ $buyer->description }}</x-form-textarea>
                         </x-form-label>
                         <x-jet-input-error for="description" />
                     </div>
@@ -67,7 +69,7 @@
                     <div class="mb-3">
                         <x-form-label>
                             <span>Address *</span>
-                            <x-form-textarea name="address">{{ old('address') }}</x-form-textarea>
+                            <x-form-textarea name="address">{{ $buyer->address }}</x-form-textarea>
                         </x-form-label>
                         <x-jet-input-error for="address" />
                     </div>
@@ -79,6 +81,8 @@
                         <x-form-label>
                             <span>Title *</span>
                             <x-form-select name="title">
+                                <option value="{{ $buyer->title }}">{{ $buyer->title }}</option>
+
                                 <option value="mr">Mr</option>
                                 <option value="mrs">Mrs</option>
                                 <option value="miss">Miss</option>
@@ -88,7 +92,7 @@
 
                         <x-form-label class=" col-span-3">
                             <span>Contacted Person *</span>
-                            <x-form-input name="contacted_person" value="{{ old('contacted_person') }}" />
+                            <x-form-input name="contacted_person" value="{{ $buyer->contacted_person }}" />
                         </x-form-label>
                         <x-jet-input-error for="contacted_person" />
                     </div>
@@ -97,21 +101,21 @@
                         <div>
                             <x-form-label>
                                 <span>Mobile *</span>
-                                <x-form-input name="mobile_1" value="{{ old('mobile_1') }}" />
+                                <x-form-input name="mobile_1" value="{{ $buyer->mobile_1 }}" />
                             </x-form-label>
                             <x-jet-input-error for="mobile_1" />
                         </div>
                         <div>
                             <x-form-label>
                                 <span>Mobile</span>
-                                <x-form-input name="mobile_2" value="{{ old('mobile_2') }}" />
+                                <x-form-input name="mobile_2" value="{{ $buyer->mobile_2 }}" />
                             </x-form-label>
                             <x-jet-input-error for="mobile_2" />
                         </div>
                         <div>
                             <x-form-label>
                                 <span>Email</span>
-                                <x-form-input name="email" value="{{ old('email') }}" />
+                                <x-form-input name="email" value="{{ $buyer->email }}" />
                             </x-form-label>
                             <x-jet-input-error for="email" />
                         </div>
