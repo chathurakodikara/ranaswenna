0d7f53af261d64d4fcde68bd249ce76427072884
samila <hasaralrandunu@gmail.com>
2020-12-30 18:19:35 +0530

    use in relationship and get to data into farmer index table

--- resources/views/livewire/farmer/index.blade.php
+++ resources/views/livewire/farmer/index.blade.php
@@ -2,7 +2,7 @@
     <div class="px-54 py-10">
         <div class=" container mx-auto px-2">
 
- 
+
                 <div class="md:flex flex-wrap justify-between items-baseline mb-3">
                     <div class="flex">
                         <h3 class=" font-bold text-gray-800 text-2xl flex-100 inline-flex items-center">Registerd Farmers List</h3>
@@ -39,7 +39,7 @@
                                         <td class="font-medium">{{ $farmer->id }}</td>
                                         <td> {{ $farmer->nic }} </td>
                                         <td> {{ $farmer->name }} </td>
-                                        <td> {{ $farmer->asc_id }} - {{ $farmer->gs_id }} </td>
+                                        <td> {{ $farmer->asc->name }} - {{ $farmer->gs->name }} </td>
                                         <td> {{ $farmer->telephone_1 }} </td>
                                         <td> {{ $farmer->telephone_2 }} </td>
 
